c707429604d6d950a8aeef27691b99cd18f72d78
jagatrajsingh2000 <jagatrajsingh2000@gmail.com>
2023-07-25 13:02:11 +0530

    code snipet pushed

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import prismaClient from "./lib/db";
+import createAppoloGraphqlServer from "./graphql";
+async function init() {
+  const app = express();
+  const PORT = Number(process.env.PORT) || 8000;
+
+  app.use(express.json());
+  app.get("/", (req, res) => {
+    res.json({ message: "Server is up and running" });
+  });
+  app.use("/graphql", expressMiddleware(await createAppoloGraphqlServer()));
+
+  app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
+}
+init();
